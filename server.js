0620afc5da7fb4bd69df7d3e5a8386df047e79ad
Thomas Watson <thomas.sm.watson@gmail.com>
2015-10-24 03:09:18 +0100

    Added AngularJs and bootstrap to Index.html

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
+
 var app = express();
+
+
 app.use(express.static('Public'));
 app.get('/',function(req,res){
-	res.use('index');
+	res.render('index');
 });
 app.listen(3000);
 console.log('Server up on port 3000');
