73c1558b0e4127c10cf78d078866c4008c7b932d
Thomas Watson <thomas.sm.watson@gmail.com>
2015-10-24 02:05:34 +0100

    Initial Commit

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+app.use(express.static('Public'));
+app.get('/',function(req,res){
+	res.use('index');
+});
+app.listen(3000);
+console.log('Server up on port 3000');
