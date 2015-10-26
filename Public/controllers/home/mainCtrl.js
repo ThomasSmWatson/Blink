0a3f529cec3c78437c7c3ac06bc04aa42fdc88b1
Thomas Watson <thomas.sm.watson@gmail.com>
2015-10-26 00:28:50 +0000

    Add more inputs to form controller front end, Add routes, linked register route

---
+++ Public/controllers/home/mainCtrl.js
@@ -0,0 +1,16 @@
+var app = angular.module('blinkApp',['ngRoute']);
+
+app.config(['$routeProvider',function($routeProvider){
+	$routeProvider.
+		when('/register',{
+			templateUrl: './../../routes/register.html',
+			controller: 'BlogController'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+}]);
+
+app.controller('BlogController',['$scope','$http',function($scope,$http){
+
+}]);
