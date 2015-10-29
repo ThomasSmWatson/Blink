c97fb60cba02d45ab0c35298cb079c386354ccc5
Thomas Watson <thomas.sm.watson@gmail.com>
2015-10-29 16:11:18 +0000

    Setup mongoose and implemented the use of config/*

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 var express = require('express');
-
+var mongoose = require('mongoose');
+var config = require('./config/node_config.json');
 var app = express();
+
+mongoose.connect('mongodb://'+config.database.address+':'+config.database.port);
+
+var db = mongoose.connection;
+
+
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', function(callback){
+	console.log('Database connected on Address :: '+ config.database.address +':'+config.database.port);
+});
 
 
 app.use(express.static('Public'));
